73cf310080ba1649e26b6abff8d6a344367953bc
Chanwut Kittivorawong <chanwutk@Chanwuts-MacBook-Pro.local>
2020-11-25 15:00:49 -0800

    format

--- src/sql-query-builder.js
+++ src/sql-query-builder.js
@@ -1,8 +1,8 @@
 import { SqlQuery } from "./sql-query";
-import {EXCEPT, INTERSECT, ORDERBY} from './constants';
-import {internal, all, op} from 'arquero';
+import { EXCEPT, INTERSECT, ORDERBY } from "./constants";
+import { internal, all, op } from "arquero";
 
-const {Verbs} = internal;
+const { Verbs } = internal;
 
 /*
 SQL execution order:
@@ -25,27 +25,32 @@ const CONFLICTS = {
   select: [SELECT, ORDERBY, SAMPLE, ...SET_OPS],
   orderby: [ORDERBY, SAMPLE, ...SET_OPS],
   dedupe: [ORDERBY, SAMPLE, ...SET_OPS],
-}
+};
 
 /**
- * 
- * @param {object[]} oldSchema 
- * @param {object[]} selection 
+ *
+ * @param {object[]} oldSchema
+ * @param {object[]} selection
  */
 function newSchema(oldSchema, selection) {
-  const fields = selection.map(s => {
-    if (s.type === 'Selection') {
-      if (s.operator === 'not') {
-        return newSchema(oldSchema, s.arguments);
-      } else if (s.operator === 'all') {
-        return oldSchema;
+  const fields = selection
+    .map((s) => {
+      if (s.type === "Selection") {
+        if (s.operator === "not") {
+          return newSchema(oldSchema, s.arguments);
+        } else if (s.operator === "all") {
+          return oldSchema;
+        }
+      } else if (s.type === "Column") {
+        return s.name;
+      } else {
+        throw new Error(
+          "Selection should only contains Selection or Column but received: ",
+          selection
+        );
       }
-    } else if (s.type === 'Column') {
-      return s.name;
-    } else {
-      throw new Error('Selection should only contains Selection or Column but received: ', selection);
-    }
-  }).flat();
+    })
+    .flat();
   return fields.filter((f, index) => fields.indexOf(f) === index);
 }
 
@@ -58,7 +63,7 @@ export class SqlQueryBuilder extends SqlQuery {
     return new SqlQueryBuilder(
       this._source,
       clauses_fn(this._clauses),
-      schema_fn(this._schema),
+      schema_fn(this._schema)
     );
   }
 
@@ -66,7 +71,7 @@ export class SqlQueryBuilder extends SqlQuery {
     return new SqlQueryBuilder(
       this,
       clauses_fn(this._clauses),
-      schema_fn(this._schema),
+      schema_fn(this._schema)
     );
   }
 
@@ -79,23 +84,30 @@ export class SqlQueryBuilder extends SqlQuery {
     if (this._schema) {
       clauses = {
         select: [
-          ...Verbs.select(this._schema).toAST().columns.map(column => {
-            const idx = newFields.find(v => v.as === column.name);
-            return idx === -1 ? column : (toKeep[idx] = false, newFields[idx]);
-          }),
-          ...newFields.filter((_, i) => toKeep[i])
-        ]
+          ...Verbs.select(this._schema)
+            .toAST()
+            .columns.map((column) => {
+              const idx = newFields.find((v) => v.as === column.name);
+              return idx === -1
+                ? column
+                : ((toKeep[idx] = false), newFields[idx]);
+            }),
+          ...newFields.filter((_, i) => toKeep[i]),
+        ],
       };
     } else {
-      clause = {select: [Verbs.select(all()).toAST().columns[0], ...newFields]};
+      clause = {
+        select: [Verbs.select(all()).toAST().columns[0], ...newFields],
+      };
     }
     return this._wrap(
       () => clauses,
-      schema => schema && [
-        ...schema,
-        newFields.filter((_, i) => toKeep[i]).map(f => f.as)
-      ]
-    )
+      (schema) =>
+        schema && [
+          ...schema,
+          newFields.filter((_, i) => toKeep[i]).map((f) => f.as),
+        ]
+    );
   }
 
   _filter(verb) {
@@ -112,34 +124,38 @@ export class SqlQueryBuilder extends SqlQuery {
   }
 
   _count(verb) {
-    const as = ('options' in verb && verb.options.as) || 'count';
-    return this.rollup(Verbs.rollup({[as]: op.count()}));
+    const as = ("options" in verb && verb.options.as) || "count";
+    return this.rollup(Verbs.rollup({ [as]: op.count() }));
   }
 
   _orderby(verb) {
-    return this._wrap(() => ({orderby: verb}), schema => schema);
+    return this._wrap(
+      () => ({ orderby: verb }),
+      (schema) => schema
+    );
   }
 
   _sample(verb) {
-    if ('options' in verb && verb.options.replace) {
+    if ("options" in verb && verb.options.replace) {
       throw new Error("sample does not support replace");
     }
-    return this
-      .derive(Verbs.derive({___arquero_sql_row_num_tmp___: op.row_number()}))
-      ._append(clauses => ({
+    return this.derive(
+      Verbs.derive({ ___arquero_sql_row_num_tmp___: op.row_number() })
+    )
+      ._append((clauses) => ({
         ...clauses,
         orderby: Verbs.orderby(op.random()),
-        limit: verb.size
+        limit: verb.size,
       }))
-      .orderby(Verbs.orderby(d => d.___arquero_sql_row_num_tmp___))
-      .select(Verbs.select(not('___arquero_sql_row_num_tmp___')))
+      .orderby(Verbs.orderby((d) => d.___arquero_sql_row_num_tmp___))
+      .select(Verbs.select(not("___arquero_sql_row_num_tmp___")));
   }
 
   _select(verb) {
     return this._wrap(
       // TODO: use newSchema if possible (SQL does not have 'not')
-      () => ({select: verb.columns}),
-      schema => newSchema(schema, verb.columns),
+      () => ({ select: verb.columns }),
+      (schema) => newSchema(schema, verb.columns)
     );
   }
 
@@ -156,57 +172,98 @@ export class SqlQueryBuilder extends SqlQuery {
   }
 
   _dedupe(verb) {
-    if (verb.keys.some(k => k.type !== 'Column')) {
-      const columns = verbs.keys.filter(k => k.type === 'Column');
+    if (verb.keys.some((k) => k.type !== "Column")) {
+      const columns = verbs.keys.filter((k) => k.type === "Column");
       const derives = verbs.keys
-        .filter(k => k.type !== 'Column')
-        .map((d, idx) => ({...d, as: `___arquero_sql_derive_tmp_${idx}___`}));
-      const deriveFields = derives.map(d => d.as);
-      return this
-        ._derive({values: derives})
+        .filter((k) => k.type !== "Column")
+        .map((d, idx) => ({ ...d, as: `___arquero_sql_derive_tmp_${idx}___` }));
+      const deriveFields = derives.map((d) => d.as);
+      return this._derive({ values: derives })
         ._append(
-          clauses => ({...clauses, distinct: [...columns.map(d => d.name), ...deriveFields]}),
-          schema => [...schema, ...deriveFields]
+          (clauses) => ({
+            ...clauses,
+            distinct: [...columns.map((d) => d.name), ...deriveFields],
+          }),
+          (schema) => [...schema, ...deriveFields]
         )
         .select(Verbs.select(not(...deriveFields)));
     } else {
       return this._wrap(
-        () => ({distinct: verb.keys.map(d => d.name)}),
-        schema => schema
+        () => ({ distinct: verb.keys.map((d) => d.name) }),
+        (schema) => schema
       );
     }
   }
 
   _concat(verb) {
     // TODO: convert verb to SqlQuery of the table
-    return this._wrap(() => ({concat: verb}), schema => schema)
+    return this._wrap(
+      () => ({ concat: verb }),
+      (schema) => schema
+    );
   }
 
   _union(verb) {
-    return this._wrap(() => ({union: verb}), schema => schema)
+    return this._wrap(
+      () => ({ union: verb }),
+      (schema) => schema
+    );
   }
 
   _intersect(verb) {
-    return this._wrap(() => ({intersect: verb}), schema => schema)
+    return this._wrap(
+      () => ({ intersect: verb }),
+      (schema) => schema
+    );
   }
 
   _except(verb) {
-    return this._wrap(() => ({except: verb}), schema => schema)
+    return this._wrap(
+      () => ({ except: verb }),
+      (schema) => schema
+    );
   }
 
-
-  derive(verb)    { return this._derive(verb.toAST()); }
-  filter(verb)    { return this._filter(verb.toAST()); }
-  groupby(verb)   { return this._groupby(verb.toAST()); }
-  rollup(verb)    { return this._rollup(verb.toAST()); }
-  count(verb)     { return this._count(verb.toAST()); }
-  orderby(verb)   { return this._orderby(verb.toAST()); }
-  sample(verb)    { return this._sample(verb.toAST()); }
-  select(verb)    { return this._select(verb.toAST()); }
-  join(verb)      { return this._join(verb.toAST()); }
-  dedupe(verb)    { return this._dedupe(verb.toAST()); }
-  concat(verb)    { return this._concat(verb.toAST()); }
-  union(verb)     { return this._union(verb.toAST()); }
-  intersect(verb) { return this._intersect(verb.toAST()); }
-  except(verb)    { return this._except(verb.toAST()); }
+  derive(verb) {
+    return this._derive(verb.toAST());
+  }
+  filter(verb) {
+    return this._filter(verb.toAST());
+  }
+  groupby(verb) {
+    return this._groupby(verb.toAST());
+  }
+  rollup(verb) {
+    return this._rollup(verb.toAST());
+  }
+  count(verb) {
+    return this._count(verb.toAST());
+  }
+  orderby(verb) {
+    return this._orderby(verb.toAST());
+  }
+  sample(verb) {
+    return this._sample(verb.toAST());
+  }
+  select(verb) {
+    return this._select(verb.toAST());
+  }
+  join(verb) {
+    return this._join(verb.toAST());
+  }
+  dedupe(verb) {
+    return this._dedupe(verb.toAST());
+  }
+  concat(verb) {
+    return this._concat(verb.toAST());
+  }
+  union(verb) {
+    return this._union(verb.toAST());
+  }
+  intersect(verb) {
+    return this._intersect(verb.toAST());
+  }
+  except(verb) {
+    return this._except(verb.toAST());
+  }
 }
