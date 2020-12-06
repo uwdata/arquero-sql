1b31f463c3dcf69228970b9a87a9bd835578616d
Chanwut Kittivorawong <chanwutk@Chanwuts-MacBook-Pro.local>
2020-12-06 04:39:41 -0800

    test all verbs

---
+++ test/sql-query-builder/concat-test.js
@@ -0,0 +1,12 @@
+import tape from 'tape';
+import {Verbs, base, deepEqualAll} from './common';
+
+tape('SqlQueryBuilder: concat', t => {
+  const concat = base.concat(Verbs.concat(['other1', 'other2']));
+  deepEqualAll(t, concat._clauses.concat, [
+    ['other1', 'should concat to other table'],
+    ['other2', 'should concat to other table']
+  ]);
+
+  t.end();
+});
---
+++ test/sql-query-builder/union-test.js
@@ -0,0 +1,12 @@
+import tape from 'tape';
+import {Verbs, base, deepEqualAll} from './common';
+
+tape('SqlQueryBuilder: union', t => {
+  const union = base.union(Verbs.union(['other1', 'other2']));
+  deepEqualAll(t, union._clauses.union, [
+    ['other1', 'should union to other table'],
+    ['other2', 'should union to other table']
+  ]);
+
+  t.end();
+});
