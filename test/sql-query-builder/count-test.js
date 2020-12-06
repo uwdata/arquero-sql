45bcbf0cbb97392042db78451f58b43da4ae033e
Chanwut Kittivorawong <chanwutk@Chanwuts-MacBook-Pro.local>
2020-12-06 03:01:36 -0800

    fix test count

--- test/sql-query-builder/count-test.js
+++ test/sql-query-builder/count-test.js
@@ -1,12 +1,15 @@
 import tape from 'tape';
 import {op} from 'arquero';
 import {Verbs, base, deepEqualAll, toAst} from './common';
+import { createColumn } from '../../src/utils';
 
 tape('SqlQueryBuilder: count', t => {
   const groupby = base.groupby(Verbs.groupby(['a', 'b']));
   const count = groupby.count(Verbs.count({as: 'count_'}));
 
   deepEqualAll(t, count._clauses.select, [
+    [createColumn('a'), 'should include groupby column'],
+    [createColumn('b'), 'should include groupby column'],
     [toAst(() => op.count(), 'count_'), 'should include the new count_ column'],
   ]);
 
