4a61f4f725d3592bb54e38ae4d972830add5aa92
Chanwut Kittivorawong <chanwutk@Chanwuts-MacBook-Pro.local>
2020-12-06 04:39:41 -0800

    test all verbs

---
+++ test/sql-query-builder/except-test.js
@@ -0,0 +1,12 @@
+import tape from 'tape';
+import {Verbs, base, deepEqualAll} from './common';
+
+tape('SqlQueryBuilder: except', t => {
+  const except = base.except(Verbs.except(['other1', 'other2']));
+  deepEqualAll(t, except._clauses.except, [
+    ['other1', 'should except to other table'],
+    ['other2', 'should except to other table']
+  ]);
+
+  t.end();
+});
