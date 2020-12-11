eff0df365267b07140a6c345e31f01add02cef34
Chanwut Kittivorawong <chanwutk@Chanwuts-MacBook-Pro.local>
2020-12-11 01:46:47 -0800

    filter-test

--- test/sql-query-builder/filter-test.js
+++ test/sql-query-builder/filter-test.js
@@ -3,40 +3,29 @@ import {op} from 'arquero';
 import {base, baseWithGroupBy, copy, toAst} from './common';
 
 tape('Sql-query-builder: filter', t => {
-  const filterForWhere = base.filter({
-    c1: d => d.a > 0,
-  });
+  const filterForWhere = base.filter([d => d.a > 0]);
 
-  const filterForHaving = baseWithGroupBy.filter({
-    c1: d => op.mean(d.a) > 0,
-    c2: d => d.b > 0,
-  });
+  const filterForHaving = baseWithGroupBy.filter([d => op.mean(d.a) > 0, d => d.b > 0]);
 
   t.deepEqual(
     copy(filterForWhere._clauses.where[0]),
-    toAst(d => d.a > 0, 'c1'),
+    toAst(d => d.a > 0, '0'),
     'Non-aggregate function add to where',
   );
 
   t.deepEqual(
     copy(filterForHaving._clauses.having[0]),
-    toAst(d => op.mean(d.a) > 0, 'c1'),
+    toAst(d => op.mean(d.a) > 0, '0'),
     'aggregate function with groupby add to having',
   );
 
   t.deepEqual(
     copy(filterForHaving._clauses.where[0]),
-    toAst(d => d.b > 0, 'c2'),
+    toAst(d => d.b > 0, '1'),
     'non-aggregate function with groupby add to where',
   );
 
-  t.throws(
-    () =>
-      base.filter({
-        c1: d => op.mean(d.a) > 0,
-      }),
-    'Cannot fillter using aggregate operations without groupby',
-  );
+  t.throws(() => base.filter([d => op.mean(d.a) > 0]), 'Cannot fillter using aggregate operations without groupby');
 
   t.end();
 });
