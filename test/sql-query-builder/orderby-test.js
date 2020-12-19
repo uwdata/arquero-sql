088761f2e4100e964429c1ad3b5a77a09864bdf1
Chanwut Kittivorawong <chanwutk@gmail.com>
2020-12-19 05:54:16 -0800

    flattened parameters

--- test/sql-query-builder/orderby-test.js
+++ test/sql-query-builder/orderby-test.js
@@ -1,19 +1,24 @@
 import tape from 'tape';
 import {desc} from 'arquero';
 import {createColumn} from '../../src/utils';
-import {base, deepEqualAll, toAst} from './common';
+import {base, copy, deepEqualAll, toAst} from './common';
 
 tape('SqlQueryBuilder: orderby', t => {
   const filter = base.filter({k: d => d.a > 0});
 
-  const orderby = filter.orderby([{a: d => d.a, b1: d => d.a + d.b, c2: desc(d => d.c)}]);
-  t.ok(orderby._source === filter, 'should nest old table');
-  deepEqualAll(t, orderby._clauses.orderby, [
+  const orderby1 = filter.orderby([{a: d => d.a, b1: d => d.a + d.b, c2: desc(d => d.c)}]);
+  t.ok(orderby1._source === filter, 'should nest old table');
+  deepEqualAll(t, orderby1._clauses.orderby, [
     [createColumn('a'), 'orderby column'],
     [toAst(d => d.a + d.b), 'orderby derived column'],
     [toAst(desc(d => d.c)), 'orderby column descending'],
   ]);
-  t.deepEqual(orderby._schema, filter._schema, 'schema should stay the same');
+  t.deepEqual(orderby1._schema, filter._schema, 'schema should stay the same');
+
+  const order2 = ['a', desc('b')];
+  const orderby2 = base.orderby(order2);
+  const orderby3 = base.orderby(...order2);
+  t.deepEqual(copy(orderby3), copy(orderby2), 'should allow flattened parameters');
 
   t.end();
 });
