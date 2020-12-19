e748d5e57e16876daa093824446f0e7985cf7c66
Chanwut Kittivorawong <chanwutk@gmail.com>
2020-12-19 05:54:16 -0800

    flattened parameters

--- test/sql-query-builder/select-test.js
+++ test/sql-query-builder/select-test.js
@@ -1,25 +1,24 @@
 import tape from 'tape';
 import {not} from 'arquero';
 import {createColumn} from '../../src/utils';
-import {base, deepEqualAll, noschema} from './common';
+import {base, copy, deepEqualAll, noschema} from './common';
+
+function includeColumn(name) {
+  return [createColumn(name), 'should include selected column'];
+}
 
 tape('SqlQueryBuilder: select', t => {
-  const select1 = base.select(['a', 'b']);
-
-  deepEqualAll(t, select1._clauses.select, [
-    [createColumn('a'), 'should include selected column'],
-    [createColumn('b'), 'should include selected column'],
-  ]);
+  const columns1 = ['a', 'b'];
+  const select1 = base.select(columns1);
+  deepEqualAll(t, select1._clauses.select, columns1.map(includeColumn));
   t.deepEqual(select1._schema.columns, ['a', 'b'], 'should produce correct schema');
 
   const select2 = base.select([not('a', 'b'), 'b']);
-
-  deepEqualAll(t, select2._clauses.select, [
-    [createColumn('c'), 'should include selected column'],
-    [createColumn('d'), 'should include selected column'],
-    [createColumn('b'), 'should include selected column'],
-  ]);
+  deepEqualAll(t, select2._clauses.select, ['c', 'd', 'b'].map(includeColumn));
   t.deepEqual(select2._schema.columns, ['c', 'd', 'b'], 'should produce correct schema');
+
+  const select4 = base.select(...columns1);
+  t.deepEqual(copy(select4), copy(select1), 'should allow flattened parameters');
 
   t.throws(() => noschema.select([not('a', 'b')]), "Cannot select with 'all' or 'not' without schema");
 
