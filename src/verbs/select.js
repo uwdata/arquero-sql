f46a8d2ea61565f7508bccc814a9248426f9c4b5
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2021-01-19 23:25:01 -0800

    select

--- src/verbs/select.js
+++ src/verbs/select.js
@@ -1,4 +1,8 @@
 /** @typedef { import('../sql-query').SqlQuery } SqlQuery */
+/** @typedef { import('../utils').ColumnType } ColumnType */
+
+import { createColumn } from '../utils';
+import resolve from './expr/selection';
 
 /**
  * 
@@ -7,7 +11,24 @@
  */
 export default function(query, verb) {
   if (!query._schema && verb.columns.some(column => column.type === 'Selection')) {
-    // TODO: match, startwith...
-    throw new Error("Cannot select with 'all' or 'not' without schema");
+    throw new Error("Cannot select with selection function(s) without schema");
   }
+
+  /** @type {ColumnType[]} */
+  const cols = [];
+  const columns = resolve(query, verb.columns);
+  columns.forEach((value, curr) => {
+    const next = typeof value === 'string' ? value : curr;
+    if (next) {
+      const col = !('_schema' in query) || query._schema?.columns.indexOf(curr) !== -1
+        ? createColumn(curr, next)
+        : error(`Unrecognized column: ${curr}`);
+      cols.push(col);
+    }
+  });
+
+  return query._wrap(
+    {select: cols},
+    {columns: cols.map(col => col.as || col.name)},
+  )
 }
