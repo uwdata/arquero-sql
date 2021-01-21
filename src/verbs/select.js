194d3d978b9fda58f8ed45c3eb907418944b2a61
Chanwut Kittivorawong <chanwutk@gmail.com>
2021-01-20 16:15:28 -0800

    union

--- src/verbs/select.js
+++ src/verbs/select.js
@@ -1,17 +1,17 @@
 /** @typedef { import('../sql-query').SqlQuery } SqlQuery */
 /** @typedef { import('../utils').ColumnType } ColumnType */
 
-import { createColumn } from '../utils';
+import {createColumn} from '../utils';
 import resolve from './expr/selection';
 
 /**
- * 
- * @param {SqlQuery} query 
- * @param {object} verb 
+ *
+ * @param {SqlQuery} query
+ * @param {object} verb
  */
-export default function(query, verb) {
+export default function (query, verb) {
   if (!query._schema && verb.columns.some(column => column.type === 'Selection')) {
-    throw new Error("Cannot select with selection function(s) without schema");
+    throw new Error('Cannot select with selection function(s) without schema');
   }
 
   /** @type {ColumnType[]} */
@@ -20,15 +20,12 @@ export default function(query, verb) {
   columns.forEach((value, curr) => {
     const next = typeof value === 'string' ? value : curr;
     if (next) {
-      const col = !('_schema' in query) || query._schema?.columns.indexOf(curr) !== -1
-        ? createColumn(curr, next)
-        : error(`Unrecognized column: ${curr}`);
-      cols.push(col);
+      if (query._schema?.columns?.indexOf(curr) === -1) {
+        throw new Error(`Unrecognized column: ${curr}`);
+      }
+      cols.push(createColumn(curr, ...(curr === next ? [] : [next])));
     }
   });
 
-  return query._wrap(
-    {select: cols},
-    {columns: cols.map(col => col.as || col.name)},
-  )
+  return query._wrap({select: cols}, {columns: cols.map(col => col.as || col.name)});
 }
