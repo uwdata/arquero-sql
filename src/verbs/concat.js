8fc838297e88e5099f52388e6e6a405a7408b2d3
Chanwut Kittivorawong <chanwutk@gmail.com>
2021-01-21 16:09:17 -0800

    combine table

--- src/verbs/concat.js
+++ src/verbs/concat.js
@@ -1,25 +1,2 @@
-/** @typedef { import('../utils').ColumnType } ColumnType */
-
-import {SqlQuery} from '../sql-query';
-import {createColumn} from '../utils';
-
-/**
- *
- * @param {SqlQuery} query
- * @param {{tables: any[]}} verb
- */
-export default function (query, verb) {
-  const {tables} = verb;
-  const select = query._schema.columns.map(col => createColumn(col));
-  const concat = tables.map(table => {
-    if (table instanceof SqlQuery) {
-      return table;
-    } else if (typeof table === 'string') {
-      return new SqlQuery(table);
-    } else {
-      throw new Error('Table must be a string or SqlQuery');
-    }
-  });
-
-  return query._wrap({select, concat}, query._schema);
-}
+import {combine} from './common';
+export default combine;
