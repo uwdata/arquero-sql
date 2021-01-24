67fba0dcd9eb4a09617a8c9a9c44aee6ff1b8c37
Chanwut Kittivorawong <chanwutk@gmail.com>
2021-01-24 04:55:29 -0800

    dedupe sample

--- src/verbs/dedupe.js
+++ src/verbs/dedupe.js
@@ -1,7 +1,7 @@
 /** @typedef {import('./common').Verb} Verb */
 /** @typedef {import('../sql-query').SqlQuery} SqlQuery */
 
-import {all, not, op} from 'arquero';
+import {not, op} from 'arquero';
 
 const TMP_COL = '___arquero_sql_temp_column_row_number___';
 
@@ -12,8 +12,8 @@ const TMP_COL = '___arquero_sql_temp_column_row_number___';
  * @returns {SqlQuery}
  */
 export default function (query, verb) {
-  // TODO: use "WITH"
+  // TODO: use "WITH" when we have the support for "with" to reduce the amount of code generated
   query = query.derive({[TMP_COL]: () => op.row_number()});
   const dedupe = query.groupby(verb.keys).rollup({[TMP_COL]: `d => op.min(d.${TMP_COL})`});
-  return query.join(dedupe, [TMP_COL, TMP_COL], [[all()], []], {suffix: ['', '_2']}).select([not(TMP_COL)]);
+  return query.join(dedupe, [TMP_COL, TMP_COL], [[not(TMP_COL)], []], {suffix: ['', '_2']});
 }
