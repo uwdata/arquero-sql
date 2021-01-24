881a20b13b1a77e9a9c3db24d07f12d854b683f0
Chanwut Kittivorawong <chanwutk@gmail.com>
2021-01-24 04:55:29 -0800

    dedupe sample

--- src/verbs/sample.js
+++ src/verbs/sample.js
@@ -1,13 +1,30 @@
 /** @typedef {import('./common').Verb} Verb */
 /** @typedef {import('../sql-query').SqlQuery} SqlQuery */
 
+import {not, op} from 'arquero';
+
+const TMP_COL = '___arquero_sql_temp_column_row_number___';
+
 /**
  *
  * @param {SqlQuery} query
  * @param {Verb} verb
  * @returns {SqlQuery}
  */
-// eslint-disable-next-line no-unused-vars
 export default function (query, verb) {
-  throw new Error('TODO: implement sample');
+  verb = verb.toAST();
+  if (verb.options && verb.options.replace) {
+    throw new Error('sample does not support replace');
+  }
+
+  if (typeof verb.size !== 'number') {
+    throw new Error('sample only support constant sample size');
+  }
+
+  return query
+    .derive({[TMP_COL]: op.row_number()})
+    .orderby([() => op.random()])
+    ._append(clauses => ({...clauses, limit: verb.size}))
+    .orderby([TMP_COL])
+    .select([not(TMP_COL)]);
 }
