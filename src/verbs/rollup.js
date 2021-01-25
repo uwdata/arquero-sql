a455f9c63fd50f0daa8ef29893785bc6397ed2d5
Chanwut Kittivorawong <chanwutk@gmail.com>
2021-01-25 04:35:17 -0800

    filter groupby join rollup

--- src/verbs/rollup.js
+++ src/verbs/rollup.js
@@ -1,13 +1,18 @@
 /** @typedef {import('./common').Verb} Verb */
 /** @typedef {import('../sql-query').SqlQuery} SqlQuery */
 
+import createColumn from '../utils/create-column';
+
 /**
  *
  * @param {SqlQuery} query
  * @param {Verb} verb
  * @returns {SqlQuery}
  */
-// eslint-disable-next-line no-unused-vars
 export default function (query, verb) {
-  throw new Error('TODO: implement rollup');
+  verb = verb.toAST();
+  const columns = new Map();
+  (query._schema.groupby || []).forEach(name => columns.set(name, createColumn(name)));
+  verb.values.forEach(value => columns.set(value.as || value.name, value));
+  return query._wrap({select: [...columns.values()]}, {columns: [...columns.keys()]});
 }
