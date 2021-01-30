685d9d1347de058b1acc4387b6a92ea61f06b5b4
Chanwut Kittivorawong <chanwutk@gmail.com>
2021-01-30 05:29:45 -0800

    use arquero's helper function

--- src/verbs/join.js
+++ src/verbs/join.js
@@ -6,6 +6,9 @@ import {inferKeys} from 'arquero/src/verbs/join';
 import {all, not} from 'arquero';
 import toArray from '../utils/to-array';
 import {internal} from 'arquero';
+import isArray from 'arquero/src/util/is-array';
+import isNumber from 'arquero/src/util/is-number';
+import isString from 'arquero/src/util/is-string';
 
 /** @type {['inner', 'left', 'right', 'outer']} */
 const JOIN_OPTIONS = ['inner', 'left', 'right', 'outer'];
@@ -13,7 +16,7 @@ const JOIN_OPTIONS = ['inner', 'left', 'right', 'outer'];
 const OPT_L = {aggregate: false, window: false};
 const OPT_R = {...OPT_L, index: 1};
 
-const optParse = { join: true, ast: true };
+const optParse = {join: true, ast: true};
 
 /**
  *
@@ -27,7 +30,7 @@ const optParse = { join: true, ast: true };
 export default function (query, other, on, values, options = {}) {
   on = inferKeys(query, other, on);
 
-  if (Array.isArray(on)) {
+  if (isArray(on)) {
     const [onL, onR] = on.map(toArray);
     if (onL.length !== onR.length) {
       throw new Error('Mismatched number of join keys');
@@ -35,8 +38,8 @@ export default function (query, other, on, values, options = {}) {
 
     const body = onL
       .map((l, i) => {
-        l = typeof l === 'number' ? query._schema.columns[l] : l;
-        const r = typeof onR[i] === 'number' ? other._schema.columns[onR[i]] : l;
+        l = isNumber(l) ? query.columnName(l) : l;
+        const r = isNumber(onR[i]) ? other.columnName(onR[i]) : l;
         return `a.${l} === b.${r}`;
       })
       .join(' && ');
@@ -50,9 +53,7 @@ export default function (query, other, on, values, options = {}) {
   }
   on = internal.parse({on}, {ast: true, join: true}).exprs[0];
 
-  console.log(values);
   const {exprs, names} = parseValues(query, other, values, optParse, options.suffix);
-  console.log(exprs, names);
   exprs.forEach((expr, i) => (expr.as = names[i]));
 
   const join_option = JOIN_OPTIONS[(~~options.left << 1) + ~~options.right];
@@ -73,16 +74,16 @@ export default function (query, other, on, values, options = {}) {
  * @param {import('arquero/src/table/transformable').JoinOptions} options
  * @returns {import('arquero/src/table/transformable').JoinKey}
  */
-function inferValues(queryL, onL, onR, options) {
+function inferValues(tableL, onL, onR, options) {
   const isect = [];
-  onL.forEach((s, i) => (typeof s === 'string' && s === onR[i] ? isect.push(s) : 0));
+  onL.forEach((s, i) => (isString(s) && s === onR[i] ? isect.push(s) : 0));
   const vR = not(isect);
 
   if (options.left && options.right) {
     // for full join, merge shared key columns together
     const shared = new Set(isect);
     return [
-      queryL._schema.columns.map(s => {
+      tableL.columnNames().map(s => {
         const c = `[${toString(s)}]`;
         return shared.has(s) ? {[s]: `(a, b) => a${c} == null ? b${c} : a${c}`} : s;
       }),
@@ -102,9 +103,12 @@ function inferValues(queryL, onL, onR, options) {
  * @param {string[]} suffix
  */
 function parseValues(tableL, tableR, values, optParse, suffix = []) {
-  if (Array.isArray(values)) {
-    let vL, vR, vJ, n = values.length;
-    vL = vR = vJ = { names: [], exprs: [] };
+  if (isArray(values)) {
+    let vL,
+      vR,
+      vJ,
+      n = values.length;
+    vL = vR = vJ = {names: [], exprs: []};
 
     if (n--) {
       vL = parseValue('join', tableL, values[0], optParse);
@@ -135,7 +139,7 @@ function parseValues(tableL, tableR, values, optParse, suffix = []) {
 
     return {
       names: vL.names.concat(vR.names, vJ.names),
-      exprs: vL.exprs.concat(vR.exprs, vJ.exprs)
+      exprs: vL.exprs.concat(vR.exprs, vJ.exprs),
     };
   } else {
     const v = internal.parse(values, optParse);
--- src/verbs/orderby.js
+++ src/verbs/orderby.js
@@ -2,6 +2,11 @@
 /** @typedef { import('./common').Verb} Verb */
 
 import {internal} from 'arquero';
+import error from 'arquero/src/util/error';
+import isFunction from 'arquero/src/util/is-function';
+import isNumber from 'arquero/src/util/is-number';
+import isObject from 'arquero/src/util/is-object';
+import isString from 'arquero/src/util/is-string';
 
 /**
  *
@@ -10,5 +15,31 @@ import {internal} from 'arquero';
  * @returns {SqlQuery}
  */
 export default function (query, keys) {
-  return query._wrap({orderby: internal.Verbs.orderby(keys).toAST().keys});
+  return query._wrap({orderby: parseValues(query, keys).exprs});
+}
+
+function parseValues(table, params) {
+  let index = -1;
+  const exprs = new Map();
+  const add = val => exprs.set(++index + '', val);
+
+  params.forEach(param => {
+    const expr = param.expr != null ? param.expr : param;
+
+    if (isObject(expr) && !isFunction(expr)) {
+      for (const key in expr) add(expr[key]);
+    } else {
+      add(
+        isNumber(expr)
+          ? `d => d.${table.columnName(expr)}`
+          : isString(expr)
+          ? `d => d.${expr}`
+          : isFunction(expr)
+          ? param
+          : error(`Invalid orderby field: ${param + ''}`),
+      );
+    }
+  });
+
+  return internal.parse(exprs, {ast: true});
 }
