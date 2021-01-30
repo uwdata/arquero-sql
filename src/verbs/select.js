5256bfd023204fe9837f6677979e94af1aad6987
Chanwut Kittivorawong <chanwutk@gmail.com>
2021-01-30 05:29:45 -0800

    use arquero's helper function

--- src/verbs/select.js
+++ src/verbs/select.js
@@ -2,7 +2,6 @@
 /** @typedef { import('../utils/create-column').ColumnType } ColumnType */
 /** @typedef { import('./common').Verb } Verb */
 
-import {internal} from 'arquero';
 import createColumn from '../utils/create-column';
 import resolve from './expr/selection';
 
@@ -13,18 +12,12 @@ import resolve from './expr/selection';
  * @returns {SqlQuery}
  */
 export default function (query, columns) {
-  const verb = internal.Verbs.select(columns).toAST();
-
-  if (!query._schema && verb.columns.some(column => column.type === 'Selection')) {
-    throw new Error('Cannot select with selection function(s) without schema');
-  }
-
   /** @type {ColumnType[]} */
   const cols = [];
-  resolve(query, verb.columns).forEach((value, curr) => {
+  resolve(query, columns).forEach((value, curr) => {
     const next = typeof value === 'string' ? value : curr;
     if (next) {
-      if (query._schema && query._schema.columns && query._schema.columns.indexOf(curr) === -1) {
+      if (!query._schema.columns.includes(curr)) {
         throw new Error(`Unrecognized column: ${curr}`);
       }
       cols.push(createColumn(curr, next));
