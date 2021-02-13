5bc99828545e33ad33739fe8fec66b0c355f159a
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2021-02-13 05:04:05 -0800

    unorder test

---
+++ test/verbs/unorder-test.js
@@ -0,0 +1,25 @@
+import tape from 'tape';
+import {base} from './common';
+
+tape('SqlQuery: unorder', t => {
+  const unorder = base.orderby('a').unorder();
+  t.deepEqual(
+    unorder._source,
+    base.orderby('a'),
+    'ungroup wraps around the previous query'
+  );
+  t.notOk(unorder._order, 'should not contain order');
+
+  t.end();
+});
+
+tape('SqlQuery: unorder a query without order', t => {
+  const unorder = base.unorder();
+  t.equal(
+    unorder,
+    base,
+    'does not make any change'
+  );
+
+  t.end();
+});
