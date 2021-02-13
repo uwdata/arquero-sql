8074e71bdeea1d18763b6757bbe67bd9d9cac10c
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2021-02-13 02:48:32 -0800

    orderby test

--- test/verbs/orderby-test.js
+++ test/verbs/orderby-test.js
@@ -1,10 +1,43 @@
 import tape from 'tape';
-import {base, pprint} from './common';
+import {desc} from 'arquero';
+import {base, copy, onlyContainClsuses} from './common';
 
 tape('SqlQuery: orderby', t => {
-  const orderby1 = base.orderby('a', d => d.b * 2, {k: d => d.a + 3});
-  // TODO: do real testing
-  pprint(orderby1);
+  const orderby = base.orderby(desc('a'), d => d.b * 2, desc({k: d => d.a + 3}));
+  onlyContainClsuses(t, orderby, []);
+  t.deepEqual(orderby._source, base, 'orderby wraps around the previous query');
+  t.deepEqual(
+    copy(orderby._order.exprs),
+    [
+      {type: 'Column', name: 'a'},
+      {
+        type: 'BinaryExpression',
+        left: {type: 'Column', name: 'b'},
+        operator: '*',
+        right: {type: 'Literal', value: 2, raw: '2'},
+      },
+      {
+        type: 'BinaryExpression',
+        left: {type: 'Column', name: 'a'},
+        operator: '+',
+        right: {type: 'Literal', value: 3, raw: '3'},
+      },
+    ],
+    'orderby annotate SqlQuery object with comparators',
+  );
+  t.deepEqual(orderby._order.descs, [true, false, true], 'orderby annotate SqlQuery object with order directions');
+
+  t.end();
+});
+
+tape('SqlQuery: orderby before other query', t => {
+  const orderby = base.orderby(desc('a')).filter(d => d.a === 1);
+  t.deepEqual(
+    copy(orderby._order.exprs),
+    [{type: 'Column', name: 'a'}],
+    'orderby annotate SqlQuery object with comparators',
+  );
+  t.deepEqual(orderby._order.descs, [true], 'orderby annotate SqlQuery object with order directions');
 
   t.end();
 });
