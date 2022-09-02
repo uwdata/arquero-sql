a3a7f31f9b21bca4741b713762741b1d98aa7aae
Chanwut (Mick) Kittivorawong <30903997+chanwutk@users.noreply.github.com>
2022-09-02 15:17:37 -0700

    demo (#18)

--- test.js
+++
@@ -1,9 +0,0 @@
-(async () => {
-    try {
-        new Promise(resolve => {
-            throw new Error('error here');
-        }).catch(e => e);
-    } catch (e) {
-        // console.log(e);
-    }
-})();
