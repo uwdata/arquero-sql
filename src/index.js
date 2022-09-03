8583136e2c7da585d5e43d2c680628268c9a830c
Chanwut (Mick) Kittivorawong <30903997+chanwutk@users.noreply.github.com>
2022-09-03 15:54:36 -0700

    Update index.js (#23)
    * Update index.js
    
    * Update index.js
    
    * fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import {ArqueroDatabase} from './databases/arquero';
 import {PostgresDatabase} from './databases/postgres';
 
 export {DBTable} from './db-table';
@@ -7,4 +8,5 @@ export {genExpr} from './databases/postgres/visitors/gen-expr';
 
 export const db = {
   Postgres: PostgresDatabase,
+  Arquero: ArqueroDatabase,
 };
