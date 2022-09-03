aa845a00a13b8a8ff345a58849d8be5f93900822
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2022-09-03 15:16:17 -0700

    test

--- src/databases/postgres/pg-table-view.js
+++ src/databases/postgres/pg-table-view.js
@@ -23,7 +23,7 @@ export class PostgresTableView extends TableView {
 
     database = database || source.database;
     if (typeof source !== 'string' && database !== source.database) {
-      console.log(source._database, database);
+      console.log(source._database === database);
       throw new Error("Database must match with parent's database");
     }
     /** @type {import('./pg-database').PostgresDatabase} */
